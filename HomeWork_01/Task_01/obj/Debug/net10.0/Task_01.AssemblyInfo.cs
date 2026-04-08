b8aac5d18adede573e27568b35b88e6316ecdac9
adettelle <113937974+adettelle@users.noreply.github.com>
2026-04-08 18:27:18 +0200

    hw1 (#1)

--- HomeWork_01/Task_01/obj/Debug/net10.0/Task_01.AssemblyInfo.cs
+++ HomeWork_01/Task_01/obj/Debug/net10.0/Task_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42cf2fc15e90fb2d9a91b6998b78dcc6b56ea4f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
