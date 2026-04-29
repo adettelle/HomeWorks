23a2955bcd19620f69dd40327c43afc197a1dfc2
adettelle <l.litvinski@gmail.com>
2026-04-29 17:36:21 +0200

    add HW_06

---
+++ HomeWork_06/HW_06_Task_04/Program.cs
@@ -0,0 +1,49 @@
+﻿// Запросите у пользователя имя, возраст и город.
+// Сформируйте строку в формате:
+// "Имя: [имя], Возраст: [возраст], Город: [город]" с использованием StringBuilder.
+
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace HW_06_Task_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name: ");
+
+            Console.Write("Enter your name: ");
+            string? name = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                Console.Write("Empty input. Try again: ");
+                name = Console.ReadLine();
+            }
+            sb.Append(name);
+            sb.Append(", Age: ");
+
+            Console.Write("Enter your age: ");
+            string? age = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(age))
+            {
+                Console.Write("Empty input. Try again: ");
+                age = Console.ReadLine();
+            }
+            sb.Append(age);
+            sb.Append(", City: ");
+
+            Console.Write("Enter your city: ");
+            string? city = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                Console.Write("Empty input. Try again: ");
+                city = Console.ReadLine();
+            }
+            sb.Append(city);
+            Console.WriteLine(sb);
+
+        }
+    }
+}
---
+++ HomeWork_06/HW_06_Task_05/Program.cs
@@ -0,0 +1,27 @@
+﻿// Составить регулярное выражение для проверки корректности номера телефона.
+// Корректный номер имеет формат +7(123)456-78-90.
+
+using System.Text.RegularExpressions;
+
+namespace HW_06_Task_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] test =
+            {
+                "+7(123)456-78-90", "+9(000)000-00-00", "-7(123)456-78-90",
+                "+K(123)456-78-90", "+7(123)456-781-901", "+77(123)456-78-90"
+            };
+
+            Regex rgx = new Regex(@"\+[0-9]\([0-9]{3}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}");
+
+            foreach (string str in test)
+            {
+                if (rgx.IsMatch(str)) Console.WriteLine($"{str} is ok");
+                else Console.WriteLine($"{str} is not ok");
+            }
+        }
+    }
+}
