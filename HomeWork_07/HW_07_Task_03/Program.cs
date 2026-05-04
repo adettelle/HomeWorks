94ca951ffd64eb2d0043925dfed27f38a0d3e5d0
adettelle <l.litvinski@gmail.com>
2026-05-04 17:08:27 +0200

    add HW_07

---
+++ HomeWork_07/HW_07_Task_03/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите метод PrintNumbers, который выводит на экран числа из массива.
+// У метода должен быть необязательный параметр reverse, который по умолчанию равен false.
+// Если reverse равен true, числа выводятся в обратном порядке.
+// Пример вызова:
+// int[] numbers = { 1, 2, 3, 4, 5 };
+// PrintNumbers(numbers); // Вывод: 1 2 3 4 5
+// PrintNumbers(numbers, true); // Вывод: 5 4 3 2 1
+namespace HW_07_Task_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            PrintNumbers(numbers); // Вывод: 1 2 3 4 5
+            PrintNumbers(numbers, true); // Вывод: 5 4 3 2 1
+        }
+
+        static void PrintNumbers(int[] nums, bool reverse = false)
+        {
+            if (reverse)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+                
+            } else
+            {
+                for (int i = nums.Length - 1; i >= 0; i--)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ HomeWork_07/HW_07_Task_05/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите перегруженные методы Multiply,
+// которые могут умножать два числа (целые или дробные) и возвращать результат.
+// Пример вызова:
+// Console.WriteLine(Multiply(2, 3)); // Вывод: 6
+// Console.WriteLine(Multiply(2.5, 3.5)); // Вывод: 8.75
+namespace HW_07_Task_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(2, 3)); // Вывод: 6
+            Console.WriteLine(Multiply(2.5, 3.5)); // Вывод: 8.75
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
