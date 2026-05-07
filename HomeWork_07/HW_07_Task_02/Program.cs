daae41beb7b924b61b3d51ca5b094d46b628bb83
adettelle <113937974+adettelle@users.noreply.github.com>
2026-05-07 18:11:22 +0200

    add HW_07 (#8)
    * add HW_07
    
    * fix HW_07

---
+++ HomeWork_07/HW_07_Task_02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Создайте метод CalcCube для вычисления объема и площади поверхности куба по длине его ребра.
+// Метод должен иметь 3 параметра - длина ребра,
+// out-параметр для объема и out-параметр для площади поверхности.
+// Пример вызова:
+// double edgeLength = 5;
+// CalcCube(edgeLength, out volume, out surfaceArea);
+// Console.WriteLine($"Объем куба: {volume}"); // Объем куба: 125
+// Console.WriteLine($"Площадь поверхности куба: {surfaceArea}"); // Площадь поверхности куба: 150
+namespace HW_07_Task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a lenght of a cube: ");
+            double edge = Convert.ToDouble(Console.ReadLine());
+
+            double area;
+            double volume;
+
+            CalcCube(edge, out area, out volume);
+            Console.WriteLine($"Объем куба: {volume:F2}"); // Объем куба: 125
+            Console.WriteLine($"Площадь поверхности куба: {area:F2}"); // Площадь поверхности куба: 150
+        }
+
+        static void CalcCube(double edge, out double area, out double volume)
+        {
+            area = Math.Pow(edge, 3);
+            volume = 6 * Math.Pow(edge, 2);
+
+        }
+    }
+}
---
+++ HomeWork_07/HW_07_Task_04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите метод FindMax, который принимает переменное количество чисел
+// и возвращает максимальное значение.
+// Используйте ключевое слово params.
+// Пример вызова:
+// int maxNumber = FindMax(10, 25, 5, 30, 15);
+// Console.WriteLine($"Max number: {maxNumber}"); // Вывод: Max number: 30
+namespace HW_07_Task_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = FindMax(10, 25, 5, 30, 15);
+            Console.WriteLine($"Max number: {maxNumber}"); // Вывод: Max number: 30
+        }
+
+        static int FindMax(params int[] nums)
+        {
+            return nums.Max();
+        }
+    }
+}
