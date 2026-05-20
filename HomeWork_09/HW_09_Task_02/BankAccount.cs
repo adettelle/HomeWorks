446e4dccbaf4f3aab056ec60c2a60091d9656e0f
adettelle <l.litvinski@gmail.com>
2026-05-20 15:15:42 +0200

    add HW_09

---
+++ HomeWork_09/HW_09_Task_02/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_09_Task_02
+{
+    internal class BankAccount
+    {
+        private static int _totalAccounts = 0;
+        public int AccountNumber { get; }
+        public decimal Balance {  get; private set; }
+            
+        public BankAccount() 
+        {
+            Random rnd = new Random();
+            AccountNumber = rnd.Next(1000, 10000); 
+            Balance = 0; 
+            _totalAccounts++;
+        }
+       
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0) throw new ArgumentException("Ammount must be positive");
+            
+            Balance += amount; 
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance < amount)
+            {
+                throw new InvalidOperationException("Not enough funds in the account");
+            }
+            if (amount <= 0) throw new ArgumentException("Ammount must be positive");
+
+            Balance -= amount;
+        }
+
+        public void ShowTotalAccounts()
+        {
+            Console.WriteLine($"Total amount of accounts: {BankAccount._totalAccounts}");
+        }
+
+        public override string ToString()
+        {
+            return $"Account: {AccountNumber}; balance: {Balance} rub.";
+        }
+    }
+}
---
+++ HomeWork_09/HW_09_Task_02/Program.cs
@@ -0,0 +1,41 @@
+﻿// Создайте класс BankAccount, который моделирует банковский счет:
+//    Статическое поле:
+//        _totalAccounts (общее количество созданных счетов, private)
+//    Автосвойства:
+//        AccountNumber (номер счета, readonly)
+//        Balance (public геттер, private сеттер)
+//    Конструктор:
+//        Увеличивает _totalAccounts и генерирует AccountNumber (например, случайный 4-значный номер).
+//    Методы:
+//        Deposit(decimal amount) – пополняет баланс.
+//        Withdraw(decimal amount) – снимает деньги (если хватает средств, иначе выбрасывает исключение).
+//        ShowTotalAccounts() - выводит общее количество созданных счетов.
+
+namespace HW_09_Task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAcc1 = new BankAccount();
+            bankAcc1.Deposit(100);
+            bankAcc1.Withdraw(50);
+            bankAcc1.ShowTotalAccounts();
+
+            var bankAcc2 = new BankAccount();
+            bankAcc2.Deposit(100);
+            try
+            {
+                bankAcc2.Withdraw(3000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"erros is: {ex.Message}");
+            }
+            bankAcc2.ShowTotalAccounts();
+
+            Console.WriteLine(bankAcc1);
+            Console.WriteLine(bankAcc2);
+        }
+    }    
+}
